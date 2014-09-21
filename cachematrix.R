2a3ef6d83b3f128323d8555951dd03fbc8f39c08
paulyapsl <paul.yap.sl@gmail.com>
2014-09-22 01:18:26 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,15 @@
 ## This function creates a special matrix that can cache the inverse of of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
+    m <- NULL ##set m (solve in this case) to NULL
+    set <- function(y) {  ##set function sets x to the argument y
       x <<- y ##assign y to x (x in the makeCacheMatrix environment)
-      m <<- NULL
+      m <<- NULL ##sets M to NULL
     }
-    get <- function() x
+    get <- function() x ##get returns the value of x (argument of MakeCacheMatrix)
     setsolve <- function(solve) m <<- solve ##m receives the value of solve
-    getsolve <- function() m
-    list(set = set, get = get,
+    getsolve <- function() m  ##getsolve returns the value of m (from makeCacheMatrix)
+    list(set = set, get = get,  ##returns a labeled vector of functions set, get, setsolve and getsolve
          setsolve = setsolve,
          getsolve = getsolve)
 }
@@ -23,13 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-    m <- x$getsolve() #calls from makeCacheMatrix
+    m <- x$getsolve() #calls from makeCacheMatrix (if it was calculated previously)
     if(!is.null(m)) {   ##if the inverse exists, retrieves from cache.
-      message("getting cached data")
-      return(m)
+      message("getting cached data") ##returns message if the inverse exists
+      return(m) ##gets value from cache and returns the value to the cacheSolve function
     }
     data <- x$get() ##if the inverse exists, retrieves from cache.
-    m <- solve(data, ...)
-    x$setsolve(m)
-    m
+    m <- solve(data, ...) ##calculates the inverse matrix
+    x$setsolve(m) ##set m in x to the calculated solve
+    m #returns m
 }
